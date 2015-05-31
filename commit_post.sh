c6e98756c0d1d815e873e9fb5561e0209c1feb88
Ben Kuhn <ben.s.kuhn@gmail.com>
2015-05-31 10:57:51 -0700

    update commit script

--- commit_post.sh
+++ commit_post.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
 OLD=_drafts/$1.md
 NEW=_posts/`date '+%Y-%m-%d'`-$1.md
+# don't commit the post if there are any TODOs
+grep TODO $OLD && echo 'found TODOs; not committing' && exit 1
 mv $OLD $NEW
 git add $NEW
 git commit -m $1
